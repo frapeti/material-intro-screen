e3f8931b137529d43929bb9f4a7b8c17c55234b4
Franco Rapetti <frapeti@gmail.com>
2018-11-19 21:50:49 -0300

    fix package name

--- material-intro-screen/src/main/java/agency/tango/materialintroscreen/widgets/CustomViewPager.java
+++ material-intro-screen/src/main/java/agency/tango/materialintroscreen/widgets/CustomViewPager.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package android.support.v4.view;
+package agency.tango.materialintroscreen.widgets;
 
 import android.content.Context;
 import android.content.res.Resources;
